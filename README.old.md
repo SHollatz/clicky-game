d84c64d514bb2b3c88d8eee14700684dffe1e64b
SHollatz <ztallohs@gmail.com>
2018-10-07 14:23:46 -0700

    deployed to gh pages

--- README.old.md
+++ README.old.md
@@ -12,3 +12,4 @@ Sabine Hollatz
 
 # License 
 MIT
+
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ const Header = (props) => (
   <div>
     <nav className="navbar">
         <span className="navbar-item">Clicky Game</span>
-        <span className="navbar-item">Click an image to begin!</span>
+        <span className="navbar-item">{props.message}</span>
         <span className="navbar-item">
           Score: {props.score} | Topscore: {props.topscore}
         </span>
