beaeba910be1ebfb891869081847d416494afa6c
SHollatz <ztallohs@gmail.com>
2019-02-16 17:14:13 -0800

    Updates

---
+++ precache-manifest.c14bb52c8ebbb16d4901d5dd54c14afa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "8b9c9e29d3704bbe1408",
+    "url": "/clicky-game/static/js/main.8b9c9e29.chunk.js"
+  },
+  {
+    "revision": "e8b01fb515c84551d48c",
+    "url": "/clicky-game/static/js/2.e8b01fb5.chunk.js"
+  },
+  {
+    "revision": "8b9c9e29d3704bbe1408",
+    "url": "/clicky-game/static/css/main.4af5502f.chunk.css"
+  },
+  {
+    "revision": "b51686911c88c46c6ee023d53232242d",
+    "url": "/clicky-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.29339bd8fad54428e7529e56418668be.js"
+  "/clicky-game/precache-manifest.c14bb52c8ebbb16d4901d5dd54c14afa.js"
 );
 
 workbox.clientsClaim();
